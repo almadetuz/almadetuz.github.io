481f6b485b55fc1d9b4f8e578630a3f6eb5b3307
Antonio Espinosa <antespi@gmail.com>
2024-04-20 13:43:13 +0200

    Images utils for subfolders too

--- convert_img.sh
+++ convert_img.sh
@@ -4,7 +4,12 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 IMG_PATH="assets/images"
 cd "$SCRIPT_DIR"
 
-for image in $IMG_PATH/*.jpg; do
+subdir=$1
+if [ -n "$subdir" ]; then
+   IMG_PATH="$IMG_PATH/$subdir"
+fi
+
+for image in "$IMG_PATH"/*.jpg; do
   base_img=`basename "${image%.jpg}"`
   webp_img="${base_img}.webp"
 
